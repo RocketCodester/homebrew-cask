31d09a7b62168d4b144d6bc4e461a1872ebc61f0
rocketcodester <micstandeven@gmail.com>
2022-03-23 14:19:44 -0500

    Update fly from 6.3.0 to 6.3.1

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,5 +1,5 @@
 cask 'fly' do
-  version '6.3.0'
+  version '6.3.1'
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/concourse/concourse/releases.atom'
